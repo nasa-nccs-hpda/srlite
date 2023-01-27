80312ac05d6e11ce9f1b9ed473a2ec0176fb9830
gtamkin <J3rgensen9!>
2023-01-26 22:05:32 -0500

    Added author

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 setup(name='srlite',
     version='1.1.0',
     description='Libraries and command-line utilities for surface reflectance',
-    author=Glenn Tamkin',
+    author='Glenn Tamkin',
     author_email='glenn.s.tamkin@nasa.gov',
     license='MIT',
     url='https://github.com/nasa-nccs-hpda/srlite',
